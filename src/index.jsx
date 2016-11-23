5165180733a2d43a1087d7801f0f40553b7a73d1
alvaro-cuesta <alvaro-cuesta@GitHub>
2016-11-23 03:13:15 +0100

    Clean import

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import _ from './index.css'
-
+import './index.css'
 import App from 'components/App'
 
 ReactDOM.render(<App />, document.getElementById('container'));
