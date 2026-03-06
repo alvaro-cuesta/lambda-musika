0aeddf808127ccb75f337be11b6f079c6d7f8b43
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2026-03-06 18:55:54 +0100

    refactor: remove unneeded index file

--- packages/musika/src/Musika/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Envelope from './Envelope.js';
-import * as Filter from './Filter/index.js';
-import * as Generator from './Generator.js';
-import * as Music from './Music.js';
-import * as Operator from './Operator.js';
-import * as Util from './Util.js';
-
-export { Envelope, Filter, Generator, Music, Operator, Util };
--- packages/musika/src/index.ts
+++ packages/musika/src/index.ts
@@ -1,2 +1,8 @@
-export * from '@lambda-musika/audio';
-export * from './Musika/index.js';
+import * as Envelope from './Envelope.js';
+import * as Filter from './Filter/index.js';
+import * as Generator from './Generator.js';
+import * as Music from './Music.js';
+import * as Operator from './Operator.js';
+import * as Util from './Util.js';
+
+export { Envelope, Filter, Generator, Music, Operator, Util };
